728a8163b4a45c64ee864d2b48d2da01f9d34ce6
Francesco Cannizzaro <francisthemoderator@gmail.com>
2016-07-28 21:29:28 +0200

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # firebase-messaging
 NodeJS module to send firebase messages
+
+# Install
+`npm install firebase-messagging`
+
+## topic(topic, data, [, callback])
+Send message to topic
+
+## message(to, data, [, callback])
+Send message to device
+ 
+# Usage
+
+```javascript
+
+const firebase = require("firebase-messaging");
+
+var client = firebase(CLOUD_MESSAGING_KEY);
+
+var data = {
+    title: "@Test",
+    content: "@Content"
+};
+
+client.topic("news", data, function(result){
+	// request err or message id
+	console.log(result);	
+});
+
+client.message(DEVICE_TOKEN, data);
+
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+const request = require("request");
+
+module.exports = function(firebase_key) {
+
+    this.options = {
+        url: "https://fcm.googleapis.com/fcm/send",
+        method: "POST",
+        headers: {
+            "Authorization": "key=" + firebase_key,
+            "Content-Type": "application/json"
+        },
+        json: {}
+    };
+
+    function send(to, data, cb) {
+
+        let options = JSON.parse(JSON.stringify(this.options));
+        options.json.to = to;
+        options.json.data = data;
+
+        request(options, function(err, response, body) {
+
+            if (cb)
+                if (body != null)
+                    cb(body);
+                else
+                    cb(err);
+
+        });
+    }
+
+    return {
+        message: function(to, data, cb) {
+            send(to, data, cb);
+        },
+        topic: function(topic, data, cb) {
+            send("/topics/" + topic, data, cb);
+        }
+    };
+
+}
